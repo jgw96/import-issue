ff15e33b2f518031c10f99da3d180905c04758a0
jgw96 <jgw9617@gmail.com>
2018-02-19 10:28:09 -0600

    first commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,40 @@
+import { Component } from '@stencil/core';
+import * as Quagga from 'quagga';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.scss'
+})
+export class AppHome {
+
+  componentDidLoad() {
+    console.log(Quagga);
+  }
+
+  render() {
+    return (
+      <ion-page class='show-page'>
+        <ion-header md-height='56px'>
+          <ion-toolbar color='primary'>
+            <ion-title>Ionic PWA Toolkit</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <p>
+            Welcome to the Ionic PWA Toolkit.
+            You can use this starter to build entire PWAs all with
+            web components using Stencil and ionic/core! Check out the readme for everything that comes in this starter out of the box and
+            Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+          </p>
+
+          <stencil-route-link url='/profile/stencil'>
+            <ion-button>
+              Profile page
+            </ion-button>
+          </stencil-route-link>
+        </ion-content>
+      </ion-page>
+    );
+  }
+}
